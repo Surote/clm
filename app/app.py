3a54c70b7675f53f744a28b4ff33c9e69bbb2f40
root <surote.z9@gmail.com>
2021-07-09 17:49:17 +0700

    change main.py to convert_excel.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
+from typing_extensions import TypeVarTuple
 from flask import Flask,send_from_directory,send_file
 from flask_autoindex import AutoIndex
 import main
+import time
 
 app = Flask(__name__)
 AutoIndex(app, browse_root='./result') 
@@ -12,3 +14,6 @@ def hello():
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=3000)
+      while True:
+            time.sleep(1000)
+            
