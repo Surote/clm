3644e4708b3970e8cb55a3538c0f215525df14b7
root <surote.z9@gmail.com>
2021-07-09 17:10:06 +0700

    integrate to flask

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
 from flask import Flask,send_from_directory,send_file
 from flask_autoindex import AutoIndex
+import main
 
 app = Flask(__name__)
-AutoIndex(app, browse_root='/home/ec2-user/NTT/clm/result') 
+AutoIndex(app, browse_root='./result') 
+
+@app.route('/run')
+def hello():
+      main.get_start()
+      return 'done'
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=3000)
