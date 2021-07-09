2499e9eaa6d595847ff9286c3ce208fbf9ec2355
root <surote.z9@gmail.com>
2021-07-09 21:38:20 +0700

    add Dockerfile

--- app/app.py
+++ app/app.py
@@ -1,19 +1,16 @@
-from typing_extensions import TypeVarTuple
 from flask import Flask,send_from_directory,send_file
 from flask_autoindex import AutoIndex
-import main
+import convert_excel
 import time
 
 app = Flask(__name__)
-AutoIndex(app, browse_root='./result') 
+AutoIndex(app, browse_root='result') 
 
 @app.route('/run')
 def hello():
-      main.get_start()
+      convert_excel.get_start()
       return 'done'
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=3000)
-      while True:
-            time.sleep(1000)
             
