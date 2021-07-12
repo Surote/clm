abac20b07f9e868e1e7ba9334b0351c266695574
root <surote.z9@gmail.com>
2021-07-12 17:11:36 +0700

    add scheduler 5 minutes run convert excel to csv

--- app/app.py
+++ app/app.py
@@ -2,16 +2,21 @@ from flask import Flask
 from flask_autoindex import AutoIndex
 import convert_excel
 from decouple import config
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 app = Flask(__name__)
 AutoIndex(app, browse_root='result') 
 
 
 @app.route('/run')
-def hello():
+def generate_csv():
       convert_excel.get_start()
       return 'Done'
 
 if __name__ == '__main__':
+      scheduler = BackgroundScheduler()
+      job = scheduler.add_job(generate_csv, 'interval', minutes=5)
+      scheduler.start()
       app.run(host='0.0.0.0', port=3001,debug=True)
             
