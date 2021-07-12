147f4521f0984c9db18dd9211cf2f64ebdad43be
root <surote.z9@gmail.com>
2021-07-12 15:59:01 +0700

    fix replace file with local variable for GL_DATA_LIST

--- app/app.py
+++ app/app.py
@@ -1,16 +1,16 @@
-from flask import Flask,send_from_directory,send_file
+from flask import Flask
 from flask_autoindex import AutoIndex
 import convert_excel
-import time
 
 app = Flask(__name__)
 AutoIndex(app, browse_root='result') 
 
+
 @app.route('/run')
 def hello():
       convert_excel.get_start()
-      return 'done'
+      return 'Done'
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=3000)
+      app.run(host='0.0.0.0', port=3001,debug=True)
             
