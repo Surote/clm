2dc006649141d319b9328d40d28b6e4df41ecae6
bubbleman1234 <ball111037@gmail.com>
2021-07-13 07:54:56 +0000

    Commit before pull from github.

--- app/app.py
+++ app/app.py
@@ -3,13 +3,16 @@ from flask_autoindex import AutoIndex
 import convert_excel
 import time
 
+
 app = Flask(__name__)
 AutoIndex(app, browse_root='result') 
+
 
 @app.route('/run')
 def hello():
       convert_excel.get_start()
       return 'done'
+
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=3000)
